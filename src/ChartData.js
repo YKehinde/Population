79a773d0dec4c165de280f01a40dbd06904a3a63
Yemi Kehinde <y.kehinde@hotmail.com>
2022-07-04 09:51:10 +0100

    changes to chart

--- src/ChartData.js
+++ src/ChartData.js
@@ -5,10 +5,7 @@ const colours = ["#ffd847", "#e0a106", "#ff47ab", "#e0064e", "#add9c0", "#1da890
 const ChartData = ({data}) => {
 
   const height = 600;
-  const width = 600;
-  const barMargin = 5;
-  // const barWidth = Math.floor(Population / 100000);
-  const barHeight = 20;
+  const width = 1000;
  
 const { Countries } = data;
 
@@ -20,7 +17,7 @@ const sortData = (data) => {
       return sortedData;
   }
 
-    const Chart = ({children, height, width}) => {
+  const Chart = ({children, height, width}) => {
     return (
       <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
         {children}
@@ -28,20 +25,49 @@ const sortData = (data) => {
     );
   };
 
-  const Bar = ({x, y, width, label, figure}) => (
-    <>
-      <text x={0} y={y}>{label}</text>
-      <rect x={x} y={y} height={barHeight} width={width} fill={getColour()} />
-      <text x={`100%`} y={y}>{figure}</text>
-    </>
+  const Bar = ({x, y, height, width, label, figure}) => (
+    <g>
+      <style>
+        {`
+        .label {
+          left: 0;
+          posation: absolute;
+        }
+        `}
+      </style>
+      <text x={0} y={y} className="label">{label}</text>
+      <rect x={100} y={y} height={height} width={width} fill={getColour()} />
+      <text x={900} y={y} className="figure">{figure}</text>
+    </g>
   );
+  
+  // const greatestValue = values => Math.max(...values);
+
+
+  const BarChart = ({data}) => {
+    const barMargin = 5;
+    const barHeight = 20;
+    // const barWidth = Math.floor(Population / 100000);
+    const sortedData = sortData(data);
+
+    return (
+      <Chart height={height} width={width}>
+        {sortedData.map((country, index) => {
+          const barWidth = 800 * (country.Population/100000);
+          const y = index * (width / sortedData.length);
+          const x = height - barMargin - barHeight;
+          const label = country.Country;
+          const figure = country.Population;
+          return <Bar x={x} y={y} height={barHeight} width={barWidth} label={label} figure={figure} />;
+        }
+        )}
+      </Chart>
+    );
+  };
+
 
   return (
-    <Chart height={height} width={width}>
-      {data && sortData(data).slice(0, 10).map((country, i) => (
-          <Bar key={i} x={0} y={i * (barHeight + barMargin)} height={barHeight} width={100} label={country.Country} figure={country.Population} />
-      ))}
-    </Chart>
+    <BarChart />
   )
 }
 
